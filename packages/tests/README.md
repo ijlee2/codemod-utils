579e327d9eb713f4a6a4ed16e7d1b7ba89bb3a3c
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-16 12:26:26 +0200

    Created @codemod-utils/tests

---
+++ packages/tests/README.md
@@ -0,0 +1,3 @@
+# @codemod-utils/tests
+
+_Utilities for tests_
---
+++ packages/tests/src/index.js
@@ -0,0 +1,9 @@
+import { strict as assert } from 'node:assert';
+
+import { test } from '@sondr3/minitest';
+
+export { assert, test };
+
+export * from './tests/assert-fixture.js';
+export * from './tests/convert-fixture-to-json.js';
+export * from './tests/load-fixture.js';
