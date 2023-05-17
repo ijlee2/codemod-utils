e4e40e1637ca7114ebf75226e5c28d57d2893461
ijlee2 <ijlee2@users.noreply.github.com>
2023-05-17 08:45:06 +0200

    bugfix: End-developer must call import.meta.url

--- packages/blueprints/src/blueprints/get-absolute-path.js
+++
@@ -1,8 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __filename = fileURLToPath(import.meta.url);
-
-export function getAbsolutePath() {
-  return dirname(__filename);
-}
