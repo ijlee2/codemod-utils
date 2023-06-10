ff565bb80eec9f3094e5c452b087759a4a7f877e
ijlee2 <ijlee2@users.noreply.github.com>
2023-06-10 09:13:52 +0200

    bugfix: Handled edge case (removeFiles)

--- packages/files/src/files/remove-files.js
+++ packages/files/src/files/remove-files.js
@@ -1,4 +1,4 @@
-import { rmSync } from 'node:fs';
+import { existsSync, rmSync } from 'node:fs';
 import { join } from 'node:path';
 
 import { removeDirectoryIfEmpty } from './remove-directory-if-empty.js';
@@ -8,6 +8,10 @@ export function removeFiles(filePaths, options) {
 
   filePaths.forEach((filePath) => {
     const source = join(projectRoot, filePath);
+
+    if (!existsSync(source)) {
+      return;
+    }
 
     rmSync(source);
     removeDirectoryIfEmpty(filePath, options);
