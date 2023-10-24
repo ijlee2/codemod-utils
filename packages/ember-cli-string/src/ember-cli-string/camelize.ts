9bebc96917dd7ad624dc2dca11957d05bd3039d7
ijlee2 <ijlee2@users.noreply.github.com>
2023-10-24 16:24:32 +0200

    refactor: Added JSDoc to @codemod-utils/ember-cli-string

--- packages/ember-cli-string/src/ember-cli-string/camelize.ts
+++ packages/ember-cli-string/src/ember-cli-string/camelize.ts
@@ -1,6 +1,25 @@
 /* https://github.com/ember-cli/ember-cli-string-utils/blob/v1.1.0/index.js */
 const STRING_CAMELIZE_REGEXP = /(-|_|\.|\s)+(.)?/g;
 
+/**
+ * Returns a string in camel-case.
+ *
+ * @param value
+ *
+ * A string.
+ *
+ * @return
+ *
+ * The string in camel case.
+ *
+ * @example
+ *
+ * ```ts
+ * const newValue = camelize('css-class-name');
+ *
+ * // 'cssClassName'
+ * ```
+ */
 export function camelize(value: string): string {
   return value
     .replace(
--- packages/ember-cli-string/src/ember-cli-string/double-colonize.ts
+++ packages/ember-cli-string/src/ember-cli-string/double-colonize.ts
@@ -1,6 +1,26 @@
-/* https://github.com/emberjs/ember.js/blob/v5.2.0/blueprints/component-test/index.js#L13-L16 */
+/* https://github.com/emberjs/ember.js/blob/v5.3.0/blueprints/component-test/index.js#L13-L16 */
 import { classify } from './classify.js';
 
+/**
+ * Returns a string associated with the angle bracket syntax
+ * for components.
+ *
+ * @param value
+ *
+ * A string (an entity name).
+ *
+ * @return
+ *
+ * The string with double colon(s) (`::`).
+ *
+ * @example
+ *
+ * ```ts
+ * const newValue = doubleColonize('ui/button');
+ *
+ * // 'Ui::Button'
+ * ```
+ */
 export function doubleColonize(value: string): string {
   const tokens = value.split('/');
 
