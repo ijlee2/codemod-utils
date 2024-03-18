97f95305c1b006ef1bc806313a35ae0842992381
ijlee2 <ijlee2@users.noreply.github.com>
2024-03-18 06:43:15 +0100

    breaking: Removed unionize

--- packages/files/tests/files/unionize/edge-case-no-file-paths.test.ts
+++
@@ -1,9 +0,0 @@
-import { assert, test } from '@codemod-utils/tests';
-
-import { unionize } from '../../../src/index.js';
-
-test('files | unionize > edge case (no file paths)', function () {
-  const pattern = unionize([]);
-
-  assert.strictEqual(pattern, '');
-});
