0ed6e3d88e10f94e9a8c6698086eaaf47ddfc502
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2024-04-22 08:34:10 +0200

    Updated dependencies (#114)
    * chore: Updated dependencies
    
    * chore: Added lockfile
    
    * chore: Ignored n/hashbang (false positive)
    
    * chore: Added changeset
    
    ---------
    
    Co-authored-by: ijlee2 <ijlee2@users.noreply.github.com>

--- packages/cli/.eslintrc.cjs
+++ packages/cli/.eslintrc.cjs
@@ -4,4 +4,12 @@ require('@shared-configs/eslint-config-node/patch');
 
 module.exports = {
   extends: ['@shared-configs/eslint-config-node/typescript'],
+  overrides: [
+    {
+      files: ['bin/**/*.{js,ts}'],
+      rules: {
+        'n/hashbang': 'off',
+      },
+    },
+  ],
 };
