19c9b305a3040cb5c6a89f22918c592b2463e8d1
ijlee2 <ijlee2@users.noreply.github.com>
2024-08-28 09:23:50 +0200

    chore: Updated fixtures

---
+++ packages/cli/tests/fixtures/javascript/output/ember-codemod-pod-to-octane/update-test-fixtures.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+#----------
+#
+#  A. Purpose
+#
+#    Fix all test fixtures after updating the source code.
+#
+#  B. Usage
+#
+#    ./update-test-fixtures.sh
+#
+#---------
+
+# Update fixtures
+rm -r "tests/fixtures/sample-project/output"
+cp -r "tests/fixtures/sample-project/input" "tests/fixtures/sample-project/output"
+
+./dist/bin/ember-codemod-pod-to-octane.js \
+  --root "tests/fixtures/sample-project/output"
