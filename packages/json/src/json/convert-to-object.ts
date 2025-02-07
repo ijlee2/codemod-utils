497440be8101db3c52c74460ae12d4b379a7e1a5
ijlee2 <ijlee2@users.noreply.github.com>
2025-02-07 12:53:53 +0100

    chore: Ignored lint errors

--- packages/json/src/json/convert-to-object.ts
+++ packages/json/src/json/convert-to-object.ts
@@ -51,5 +51,6 @@
 export function convertToObject(map = new Map()) {
   const sortedMap = new Map([...map.entries()].sort());
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return Object.fromEntries(sortedMap);
 }
