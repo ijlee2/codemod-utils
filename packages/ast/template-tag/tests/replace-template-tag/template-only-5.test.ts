37226d40fc3afd30377650f78ef44906876abb93
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-10 21:45:31 +0200

    Handled <template> tag in ternary operators (#197)
    * refactor, feature: Removed unnecessary code. Handled <template> tag in ternary operators.
    
    * refactor: Added tests
    
    * chore: Added changeset

---
+++ packages/ast/template-tag/tests/replace-template-tag/template-only-5.test.ts
@@ -0,0 +1,76 @@
+import { assert, test } from '@codemod-utils/tests';
+
+import { replaceTemplateTag } from '../../src/index.js';
+
+test('replace-template-tag > template-only (5)', function () {
+  const oldFile = [
+    `import type { TOC } from '@ember/component/template-only';`,
+    `import { isTesting, macroCondition } from '@embroider/macros';`,
+    ``,
+    `import styles from './image.css';`,
+    ``,
+    `interface ProductsProductImageSignature {`,
+    `  Args: {`,
+    `    src: string;`,
+    `  };`,
+    `}`,
+    ``,
+    `const ProductsProductImageComponent: TOC<ProductsProductImageSignature> =`,
+    `  macroCondition(isTesting())`,
+    `    ? <template>`,
+    `        <div class={{styles.placeholder-image}}></div>`,
+    `      </template>`,
+    `    : <template><img alt="" class={{styles.image}} src={{@src}} /></template>;`,
+    ``,
+    `export default ProductsProductImageComponent;`,
+    ``,
+    `declare module '@glint/environment-ember-loose/registry' {`,
+    `  export default interface Registry {`,
+    `    'Products::Product::Image': typeof ProductsProductImageComponent;`,
+    `    'products/product/image': typeof ProductsProductImageComponent;`,
+    `  }`,
+    `}`,
+    ``,
+  ].join('\n');
+
+  const newFile = replaceTemplateTag(oldFile, {
+    code: '<template>New contents</template>',
+    range: {
+      endByte: 428,
+      endChar: 428,
+      startByte: 345,
+      startChar: 345,
+    },
+  });
+
+  assert.strictEqual(
+    newFile,
+    [
+      `import type { TOC } from '@ember/component/template-only';`,
+      `import { isTesting, macroCondition } from '@embroider/macros';`,
+      ``,
+      `import styles from './image.css';`,
+      ``,
+      `interface ProductsProductImageSignature {`,
+      `  Args: {`,
+      `    src: string;`,
+      `  };`,
+      `}`,
+      ``,
+      `const ProductsProductImageComponent: TOC<ProductsProductImageSignature> =`,
+      `  macroCondition(isTesting())`,
+      `    ? <template>New contents</template>`,
+      `    : <template><img alt="" class={{styles.image}} src={{@src}} /></template>;`,
+      ``,
+      `export default ProductsProductImageComponent;`,
+      ``,
+      `declare module '@glint/environment-ember-loose/registry' {`,
+      `  export default interface Registry {`,
+      `    'Products::Product::Image': typeof ProductsProductImageComponent;`,
+      `    'products/product/image': typeof ProductsProductImageComponent;`,
+      `  }`,
+      `}`,
+      ``,
+    ].join('\n'),
+  );
+});
