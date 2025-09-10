e746e1791513bde4f9b802708c638ea54d8faf6a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-10 12:18:02 +0200

    feature: Handled <template> tag inside a class

--- packages/ast/template-tag/src/to-template-tag.ts
+++ packages/ast/template-tag/src/to-template-tag.ts
@@ -1,5 +1,28 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 import { AST } from '@codemod-utils/ast-javascript';
+
+const MARKER = 'template_fd9b2463e5f141cfb5666b64daa1f11a';
+
+function getTemplateTag(expression: unknown): string | undefined {
+  // @ts-expect-error: Incorrect type
+  if (expression.type !== 'CallExpression') {
+    return;
+  }
+
+  if (
+    // @ts-expect-error: Incorrect type
+    expression.callee.type !== 'Identifier' ||
+    // @ts-expect-error: Incorrect type
+    expression.callee.name !== MARKER
+  ) {
+    return;
+  }
+
+  // @ts-expect-error: Incorrect type
+  const template = expression.arguments[0].quasis[0].value.raw as string;
+
+  return `<template>${template}</template>`;
+}
 
 export function toTemplateTag(ecma: string): string {
   const traverse = AST.traverse(true);
@@ -15,6 +38,22 @@ export function toTemplateTag(ecma: string): string {
 
       // For simplicity, always remove the import statement
       return null;
+    },
+
+    visitStaticBlock(node) {
+      const bodyNode = node.value.body[0];
+
+      if (bodyNode.type !== 'ExpressionStatement') {
+        return false;
+      }
+
+      const templateTag = getTemplateTag(bodyNode.expression);
+
+      if (templateTag === undefined) {
+        return false;
+      }
+
+      return templateTag;
     },
   });
 
