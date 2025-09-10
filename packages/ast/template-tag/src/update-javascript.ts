8a7d964fa287e6ee93d8ab28dd440f1e87f166c6
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-09-10 18:13:15 +0200

    bugfix: Removed naive approach

--- packages/ast/template-tag/src/update-javascript.ts
+++ packages/ast/template-tag/src/update-javascript.ts
@@ -1,40 +1,5 @@
-const MARKER = 'template_fd9b2463e5f141cfb5666b64daa1f11a';
-
-function markTemplateTags(file: string): string {
-  const lines = file.split('\n');
-
-  return lines
-    .reduce<string[]>((accumulator, line) => {
-      if (line.includes('<template>')) {
-        accumulator.push(`/* ${MARKER}`);
-      }
-
-      accumulator.push(line);
-
-      if (line.includes('</template>')) {
-        accumulator.push(`${MARKER} */`);
-      }
-
-      return accumulator;
-    }, [])
-    .join('\n');
-}
-
-function unmarkTemplateTags(file: string): string {
-  const lines = file.split('\n');
-
-  return lines
-    .reduce<string[]>((accumulator, line) => {
-      if (line.includes(`/* ${MARKER}`) || line.includes(`${MARKER} */`)) {
-        return accumulator;
-      }
-
-      accumulator.push(line);
-
-      return accumulator;
-    }, [])
-    .join('\n');
-}
+import { toEcma } from './to-ecma.js';
+import { toTemplateTag } from './to-template-tag.js';
 
 /**
  * Updates the JavaScript part of a file. Leaves the `<template>`
@@ -90,7 +55,7 @@ export function updateJavaScript(
   file: string,
   update: (code: string) => string,
 ): string {
-  file = update(markTemplateTags(file));
+  file = update(toEcma(file));
 
-  return unmarkTemplateTags(file);
+  return toTemplateTag(file);
 }
