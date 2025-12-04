0712be4da76803dbad9e7df769a3169eed2bb37a
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2025-12-04 11:39:35 -0600

    chore: Updated tests to pass on Windows (ast-template)

--- packages/ast/template/tests/index/traverse-base-case.test.ts
+++ packages/ast/template/tests/index/traverse-base-case.test.ts
@@ -1,26 +1,24 @@
-import { assert, test } from '@codemod-utils/tests';
+import { assert, createFile, test } from '@codemod-utils/tests';
 
 import { traverseHandlebars } from '../helpers/index.js';
 
 test('index > traverse (base case)', function () {
-  const oldFile = [
+  const oldFile = createFile([
     `{{! Some comment }}`,
     `<div data-test-container local-class="container">`,
     `  {{! Some content }}`,
     `</div>`,
-    ``,
-  ].join('\n');
+  ]);
 
   const newFile = traverseHandlebars(oldFile);
 
   assert.strictEqual(
     newFile,
-    [
+    createFile([
       `{{! Some comment }}`,
       `<div data-test-container local-class="container">`,
       `  {{! Some content }}`,
       `</div>`,
-      ``,
-    ].join('\n'),
+    ]),
   );
 });
