9f4ccc279a8802601119b682cc0eda2072041427
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-03-05 20:56:17 +0100

    chore: Documented public API

--- packages/threads/src/run-task.ts
+++ packages/threads/src/run-task.ts
@@ -4,6 +4,51 @@ const MAX_NUM_TASKS_RUNNING = 10;
 
 export type Task<T extends unknown[], U> = (...dataset: T) => U | Promise<U>;
 
+/**
+ * Runs a task on many datasets. The size of datasets should be moderate.
+ * Primarily used to create a worker file for a task.
+ *
+ * Note, a worker file always uses the code shown below. You just need to
+ * import the task from the right file.
+ *
+ * @param task
+ *
+ * Some function to call.
+ *
+ * @param datasets
+ *
+ * An array of dataset's.
+ *
+ * @return
+ *
+ * An array of the task's return value.
+ *
+ * @example
+ *
+ * Create a worker file for a task.
+ *
+ * ```ts
+ * import { parentPort, workerData } from 'node:worker_threads';
+ *
+ * import { runTask } from '@codemod-utils/threads';
+ *
+ * import { task } from './task.js';
+ *
+ * type WorkerData = {
+ *   datasets: Parameters<typeof task>[];
+ * };
+ *
+ * const { datasets } = workerData as WorkerData;
+ *
+ * runTask(task, datasets)
+ *   .then((result) => {
+ *     parentPort?.postMessage(result);
+ *   })
+ *   .catch((error) => {
+ *     throw error;
+ *   });
+ * ```
+ */
 export async function runTask<T extends unknown[], U>(
   task: Task<T, U>,
   datasets: T[],
