e7fda67032b9a979d84e64b73edea34a1e4810bd
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-05-04 07:32:13 +0200

    Updated latestVersions (#308)
    * chore: Updated latestVersions
    
    * chore: Added changeset

--- packages/cli/src/utils/blueprints/get-version.ts
+++ packages/cli/src/utils/blueprints/get-version.ts
@@ -2,9 +2,9 @@ import { decideVersion } from '@codemod-utils/blueprints';
 
 const latestVersions = new Map([
   ['@changesets/cli', '2.31.0'],
-  ['@codemod-utils/ast-javascript', '3.0.1'],
+  ['@codemod-utils/ast-javascript', '3.0.2'],
   ['@codemod-utils/ast-template', '3.0.1'],
-  ['@codemod-utils/ast-template-tag', '2.2.0'],
+  ['@codemod-utils/ast-template-tag', '2.3.0'],
   ['@codemod-utils/blueprints', '3.1.0'],
   ['@codemod-utils/ember', '4.1.1'],
   ['@codemod-utils/files', '4.0.1'],
