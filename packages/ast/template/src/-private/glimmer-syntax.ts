0412d7fe261ae0f3dc5b5366a44dc64e1ffc7d2b
Isaac Lee <16869656+ijlee2@users.noreply.github.com>
2026-05-06 09:26:16 +0200

    Revert "breaking: Consumed @glimmer/syntax directly"
    This reverts commit d9abca95be52b2015de88ddd52415ea01e702609.

--- packages/ast/template/src/-private/glimmer-syntax.ts
+++ packages/ast/template/src/-private/glimmer-syntax.ts
@@ -2,30 +2,35 @@ import {
   type AST,
   builders,
   type NodeVisitor,
-  preprocess,
   print as upstreamPrint,
   traverse as upstreamTraverse,
 } from '@glimmer/syntax';
 
-export function parse(file: string): AST.Template {
-  const ast = preprocess(file, {
-    mode: 'codemod',
-  });
+import { type NodeInfo, Parser } from './glimmer-syntax/parser.js';
 
-  return ast;
+const NODE_INFO = new WeakMap<AST.Node, NodeInfo>();
+
+export function parse(template: string): AST.Template {
+  return new Parser(template, NODE_INFO).ast;
 }
 
 export function print(ast: AST.Node): string {
   return upstreamPrint(ast, {
     entityEncoding: 'raw',
+    // @ts-expect-error: Incorrect type
+    override: (ast) => {
+      const info = NODE_INFO.get(ast);
+
+      if (info) {
+        return info.parse_result.print(ast);
+      }
+    },
   });
 }
 
 export function traverse() {
   return function (file: string, visitMethods: NodeVisitor = {}): AST.Template {
-    const ast = preprocess(file, {
-      mode: 'codemod',
-    });
+    const { ast } = new Parser(file, NODE_INFO);
 
     upstreamTraverse(ast, visitMethods);
 
