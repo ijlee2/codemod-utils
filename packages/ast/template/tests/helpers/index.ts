2fba4f63c163988a2e3d6e91b661fa5e324bbfb9
ijlee2 <ijlee2@users.noreply.github.com>
2026-05-28 10:03:23 +0200

    chore: Standardized tests

--- packages/ast/template/tests/helpers/index.ts
+++ packages/ast/template/tests/helpers/index.ts
@@ -1,1 +1,33 @@
-export * from './handlebars.js';
+import { AST } from '../../src/index.js';
+
+export function transform(file: string): string {
+  const traverse = AST.traverse();
+
+  const ast = traverse(file, {
+    AttrNode(node) {
+      if (node.name !== 'local-class') {
+        return;
+      }
+
+      if (node.value.type === 'TextNode') {
+        node.name = 'class';
+
+        const attributeValue = node.value.chars.trim();
+
+        node.value = AST.builders.mustache(
+          AST.builders.path(`this.styles.${attributeValue}`),
+        );
+      }
+    },
+  });
+
+  return AST.print(ast);
+}
+
+export function traverse(file: string): string {
+  const traverse = AST.traverse();
+
+  const ast = traverse(file);
+
+  return AST.print(ast);
+}
--- packages/ast/template/tests/index/transform-file-is-empty.test.ts
+++ packages/ast/template/tests/index/transform-file-is-empty.test.ts
@@ -1,11 +1,11 @@
 import { assert, test } from '@codemod-utils/tests';
 
-import { transformHandlebars } from '../helpers/index.js';
+import { transform } from '../helpers/index.js';
 
 test('index > transform (file is empty)', function () {
   const oldFile = '';
 
-  const newFile = transformHandlebars(oldFile);
+  const newFile = transform(oldFile);
 
   assert.strictEqual(newFile, '');
 });
