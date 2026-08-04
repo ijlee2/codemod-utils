da85ad49e17341f27a89d7775b9fcfd51f2302ba
ijlee2 <ijlee2@users.noreply.github.com>
2026-08-04 09:15:44 +0200

    chore: Updated latestVersions

--- packages/cli/src/utils/blueprints/get-version.ts
+++ packages/cli/src/utils/blueprints/get-version.ts
@@ -2,7 +2,7 @@ import { decideVersion } from '@codemod-utils/blueprints';
 
 const latestVersions = new Map([
   ['@changesets/cli', '2.31.1'],
-  ['@codemod-utils/ast-javascript', '4.2.1'],
+  ['@codemod-utils/ast-javascript', '4.3.0'],
   ['@codemod-utils/ast-template', '4.1.0'],
   ['@codemod-utils/ast-template-tag', '2.7.0'],
   ['@codemod-utils/blueprints', '3.2.0'],
@@ -21,7 +21,7 @@ const latestVersions = new Map([
   ['@types/yargs', '17.0.35'],
   ['concurrently', '10.0.4'],
   ['eslint', '10.8.0'],
-  ['pnpm', '11.17.0'],
+  ['pnpm', '11.20.0'],
   ['prettier', '3.9.6'],
   ['typescript', '6.0.3'],
   ['yargs', '18.0.0'],
